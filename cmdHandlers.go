34c8dc21fb93d60997dd03061916d79f34a707ac
0xBahamoot <lam@autonomous.nyc>
2021-01-14 22:37:29 +0700

    update

---
+++ cmdHandlers.go
@@ -0,0 +1,243 @@
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	"encoding/hex"
+	"errors"
+	"fmt"
+
+	"gitlab.com/NebulousLabs/Sia/types"
+)
+
+func (n *NanoS) GetVersion() (version string, err error) {
+	resp, err := n.Exchange(cmdGetVersion, 0, 0, nil)
+	if err != nil {
+		return "", err
+	} else if len(resp) != 3 {
+		fmt.Printf("%v\n", resp)
+		return "", errors.New("version has wrong length")
+	}
+	return fmt.Sprintf("v%d.%d.%d", resp[0], resp[1], resp[2]), nil
+}
+
+func (n *NanoS) GetAddress(index uint32) (addr types.UnlockHash, err error) {
+	encIndex := make([]byte, 4)
+	binary.LittleEndian.PutUint32(encIndex, index)
+
+	resp, err := n.Exchange(cmdGetAddress, 0, 0, nil)
+	if err != nil {
+		return types.UnlockHash{}, err
+	}
+	fmt.Printf("address %v\n", resp[:])
+	fmt.Println("\n", string(resp[:]))
+	// fmt.Println("address:", string(resp))
+	return
+}
+
+func (n *NanoS) GetPrivateKey(index uint32) (addr types.UnlockHash, err error) {
+	encIndex := make([]byte, 4)
+	binary.LittleEndian.PutUint32(encIndex, index)
+
+	resp, err := n.Exchange(cmdGetPrivateKey, 0, p2DisplayAddress, encIndex)
+	if err != nil {
+		return types.UnlockHash{}, err
+	}
+	fmt.Println("privatekey:", string(resp))
+	// err = addr.LoadString(string(resp[32:]))
+	return
+}
+
+func (n *NanoS) GetViewKey() error {
+	resp, err := n.Exchange(cmdGetViewKey, 0, 0, nil)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("viewkey: %v", resp)
+	// fmt.Println("viewkey:", string(resp[:]))
+	return nil
+}
+
+func (n *NanoS) GetOTAKey() error {
+	resp, err := n.Exchange(cmdGetOTAKey, 0, 0, nil)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("ota: %v", resp)
+	// fmt.Println("viewkey:", string(resp[:]))
+	return nil
+}
+
+func (n *NanoS) ImportPrivateKey() error {
+	buf := new(bytes.Buffer)
+	// 000100000020812566598706f6f772fa0ec67e5efaac12c85a64b730518077a432fd3cb97a8c20063632b2a159e45002394460aee02de54d2b8926d236f45be2e077dcc81d0d04
+
+	bs, _ := hex.DecodeString("00000000000214666ccc56b88d4d8d3f5fae61f1f06d9620327fe259157272016dfe54ef6fef20a408be78955356a9d3aef1729c6d83d32f91ea84cf21a974d2d9d791d71e1c06")
+	buf.Write(bs)
+	// buf.WriteString("111111bgk2j6vZQvzq8tkonDLLXEvLkMwBMn5BoLXLpf631boJnJ1UgJnLBzXe4qSMXGJAKw1LdKmfWZDNkhd24gkb2oqbs4q9UgjJZDvq")
+
+	resp, err := n.Exchange(cmdImportPrivateKey, 0, 0, buf.Next(255))
+	if err != nil {
+		return err
+	}
+	_ = resp
+	return nil
+}
+
+func (n *NanoS) GenCommitment() error {
+	resp, err := n.Exchange(cmdGenCommitment, 0, 0, nil)
+	if err != nil {
+		return err
+	}
+	_ = resp
+	return nil
+}
+
+func (n *NanoS) GenOTA() error {
+	resp, err := n.Exchange(cmdGenOTA, 0, 0, nil)
+	if err != nil {
+		return err
+	}
+	_ = resp
+	return nil
+}
+
+func (n *NanoS) GenRingSig() error {
+	resp, err := n.Exchange(cmdGenRingSig, 0, 0, nil)
+	if err != nil {
+		return err
+	}
+	_ = resp
+	return nil
+}
+
+func (n *NanoS) GenProof() error {
+	resp, err := n.Exchange(cmdGenProof, 0, 0, nil)
+	if err != nil {
+		return err
+	}
+	_ = resp
+	return nil
+}
+
+func (n *NanoS) GenAssetTag() error {
+	resp, err := n.Exchange(cmdGenAssetTag, 0, 0, nil)
+	if err != nil {
+		return err
+	}
+	_ = resp
+	return nil
+}
+
+func (n *NanoS) GenKeyImage() error {
+	buf := new(bytes.Buffer)
+	bs, _ := hex.DecodeString("c4541151e39bb43e7b00ad6a1d999d609f5939ca622a9db7b7391c5190eea909")
+	buf.Write(bs)
+	bs1, _ := hex.DecodeString("17fd6aff8fecd18243af1a83dab0e47ca5fafec256ba497b3136a6b3f68eecb1")
+	buf.Write(bs1)
+
+	resp, err := n.Exchange(cmdKeyImage, 0, 0, buf.Next(255))
+	if err != nil {
+		return err
+	}
+	// fmt.Println("coin_pubkey", hex.EncodeToString(resp[:32]))
+	// fmt.Println("I", hex.EncodeToString(resp[32:len(resp)-1]))
+	fmt.Println("I", hex.EncodeToString(resp))
+	return nil
+}
+
+func (n *NanoS) EncryptCoin() error {
+	resp, err := n.Exchange(cmdEncryptCoin, 0, 0, nil)
+	if err != nil {
+		return err
+	}
+	_ = resp
+	return nil
+}
+
+func (n *NanoS) DecryptCoin() error {
+	resp, err := n.Exchange(cmdDecryptCoin, 0, 0, nil)
+	if err != nil {
+		return err
+	}
+	_ = resp
+	return nil
+}
+
+func (n *NanoS) GetValidatorKey() error {
+	resp, err := n.Exchange(cmdGetValidatorKey, 0, 0, nil)
+	if err != nil {
+		return err
+	}
+	_ = resp
+	return nil
+}
+
+func (n *NanoS) TrustDevice() error {
+	resp, err := n.Exchange(cmdTrustDevice, 0, 0, nil)
+	if err != nil {
+		return err
+	}
+	_ = resp
+	return nil
+}
+
+// func (n *NanoS) SignHash(hash [32]byte, keyIndex uint32) (sig [64]byte, err error) {
+// 	encIndex := make([]byte, 4)
+// 	binary.LittleEndian.PutUint32(encIndex, keyIndex)
+
+// 	resp, err := n.Exchange(cmdSignHash, 0, 0, append(encIndex, hash[:]...))
+// 	if err != nil {
+// 		return [64]byte{}, err
+// 	}
+// 	if copy(sig[:], resp) != len(sig) {
+// 		return [64]byte{}, errors.New("signature has wrong length")
+// 	}
+// 	return
+// }
+
+// func (n *NanoS) CalcTxnHash(txn types.Transaction, sigIndex uint16) (hash [32]byte, err error) {
+// 	buf := new(bytes.Buffer)
+// 	binary.Write(buf, binary.LittleEndian, uint32(0)) // keyIndex
+// 	binary.Write(buf, binary.LittleEndian, sigIndex)
+// 	txn.MarshalSia(buf)
+
+// 	var resp []byte
+// 	for buf.Len() > 0 {
+// 		var p1 byte = p1More
+// 		if resp == nil {
+// 			p1 = p1First
+// 		}
+// 		resp, err = n.Exchange(cmdCalcTxnHash, p1, p2DisplayHash, buf.Next(255))
+// 		if err != nil {
+// 			return [32]byte{}, err
+// 		}
+// 	}
+// 	if copy(hash[:], resp) != len(hash) {
+// 		return [32]byte{}, errors.New("hash has wrong length")
+// 	}
+// 	return
+// }
+
+// func (n *NanoS) SignTxn(txn types.Transaction, sigIndex uint16, keyIndex uint32) (sig [64]byte, err error) {
+// 	buf := new(bytes.Buffer)
+// 	binary.Write(buf, binary.LittleEndian, keyIndex)
+// 	binary.Write(buf, binary.LittleEndian, sigIndex)
+// 	txn.MarshalSia(buf)
+
+// 	var resp []byte
+// 	for buf.Len() > 0 {
+// 		var p1 byte = p1More
+// 		if resp == nil {
+// 			p1 = p1First
+// 		}
+// 		resp, err = n.Exchange(cmdCalcTxnHash, p1, p2SignHash, buf.Next(255))
+// 		if err != nil {
+// 			return [64]byte{}, err
+// 		}
+// 	}
+// 	if copy(sig[:], resp) != len(sig) {
+// 		return [64]byte{}, errors.New("signature has wrong length")
+// 	}
+// 	return
+// }
---
+++ main.go
@@ -0,0 +1,254 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"strconv"
+
+	"github.com/karalabe/hid"
+	"lukechampine.com/flagg"
+)
+
+func OpenNanoS() (*NanoS, error) {
+	const (
+		ledgerVendorID       = 0x2c97
+		ledgerNanoSProductID = 0x0001
+		//ledgerUsageID        = 0xffa0
+	)
+
+	// search for Nano S
+	devices := hid.Enumerate(ledgerVendorID, ledgerNanoSProductID)
+	if len(devices) == 0 {
+		return nil, errors.New("Nano S not detected")
+	} // else if len(devices) > 1 {
+	// 	return nil, errors.New("Unexpected error -- Is the Incognito wallet app running?")
+	// }
+
+	// open the device
+	device, err := devices[0].Open()
+	if err != nil {
+		return nil, err
+	}
+
+	// wrap raw device I/O in HID+APDU protocols
+	return &NanoS{
+		device: &apduFramer{
+			hf: &hidFramer{
+				rw: device,
+			},
+		},
+	}, nil
+}
+
+func parseIndex(s string) uint32 {
+	index, err := strconv.ParseUint(s, 10, 32)
+	if err != nil {
+		log.Fatalln("Couldn't parse index:", err)
+	} else if index > math.MaxUint32 {
+		log.Fatalf("Index too large (max %v)", math.MaxUint32)
+	}
+	return uint32(index)
+}
+
+const (
+	rootUsage = `Usage:
+    incognitoledger [flags] [action]
+
+Actions:
+    addr            generate an address
+    pubkey          generate a pubkey
+    hash            sign a trusted hash
+    txn             sign a transaction
+`
+	debugUsage = `print raw APDU exchanges`
+
+	versionUsage = `Usage:
+	sialedger version
+
+Prints the version of the sialedger binary, as well as the version reported by
+the Sia Ledger Nano S app (if available).
+`
+	addrUsage = `Usage:
+	sialedger addr [key index]
+
+Generates an address using the public key with the specified index.
+`
+	privUsage          = ``
+	viewKeyUsage       = ``
+	importPrivUsage    = ``
+	genCommitmentUsage = ``
+	genOTAUsage        = ``
+	genRingSigUsage    = ``
+	genProofUsage      = ``
+	genAssetTagUsage   = ``
+	genKeyImageUsage   = ``
+	encryptCoinUsage   = ``
+	decryptCoinUsage   = ``
+	getValidatorUsage  = ``
+	getOTAKeyUsage     = ``
+	trustDeviceUsage   = ``
+)
+
+func main() {
+	log.SetFlags(0)
+	rootCmd := flagg.Root
+	rootCmd.Usage = flagg.SimpleUsage(rootCmd, rootUsage)
+	rootCmd.BoolVar(&DEBUG, "apdu", false, debugUsage)
+
+	versionCmd := flagg.New("version", versionUsage)
+	addrCmd := flagg.New("addr", addrUsage)
+	privCmd := flagg.New("priv", privUsage)
+	getViewKeyCmd := flagg.New("view", viewKeyUsage)
+	importPrivateKeyCmd := flagg.New("importpriv", importPrivUsage)
+	genCommitmentCmd := flagg.New("gencommitment", genCommitmentUsage)
+	genOTACmd := flagg.New("genota", genOTAUsage)
+	genRingSigCmd := flagg.New("genringsig", genRingSigUsage)
+	genProofCmd := flagg.New("genproof", genProofUsage)
+	genAssetTagCmd := flagg.New("genassettag", genAssetTagUsage)
+	genKeyImageCmd := flagg.New("genkeyimage", genKeyImageUsage)
+	encryptCoinCmd := flagg.New("encoin", encryptCoinUsage)
+	decryptCoinCmd := flagg.New("decoin", decryptCoinUsage)
+	getValidatorCmd := flagg.New("getvalidator", getValidatorUsage)
+	getOTAKeyCmd := flagg.New("ota", getOTAKeyUsage)
+	trustDeviceCmd := flagg.New("trust", trustDeviceUsage)
+	cmd := flagg.Parse(flagg.Tree{
+		Cmd: rootCmd,
+		Sub: []flagg.Tree{
+			{Cmd: versionCmd},
+			{Cmd: addrCmd},
+			{Cmd: privCmd},
+			{Cmd: getViewKeyCmd},
+			{Cmd: importPrivateKeyCmd},
+			{Cmd: genCommitmentCmd},
+			{Cmd: genOTACmd},
+			{Cmd: genRingSigCmd},
+			{Cmd: genProofCmd},
+			{Cmd: genAssetTagCmd},
+			{Cmd: genKeyImageCmd},
+			{Cmd: encryptCoinCmd},
+			{Cmd: decryptCoinCmd},
+			{Cmd: getValidatorCmd},
+			{Cmd: getOTAKeyCmd},
+			{Cmd: trustDeviceCmd},
+		},
+	})
+	args := cmd.Args()
+
+	var nanos *NanoS
+	if cmd != rootCmd && cmd != versionCmd {
+		var err error
+		nanos, err = OpenNanoS()
+		if err != nil {
+			log.Fatalln("Couldn't open device:", err)
+		}
+	}
+
+	switch cmd {
+	case rootCmd:
+		if len(args) != 0 {
+			rootCmd.Usage()
+			return
+		}
+		fallthrough
+	case versionCmd:
+		// try to get Nano S app version
+		var appVersion string
+		nanos, err := OpenNanoS()
+		if err != nil {
+			appVersion = "(could not connect to Nano S)"
+		} else if appVersion, err = nanos.GetVersion(); err != nil {
+			appVersion = "(could not read version from Nano S: " + err.Error() + ")"
+		}
+
+		fmt.Printf("%s v0.1.0\n", os.Args[0])
+		fmt.Println("Nano S app version:", appVersion)
+	case addrCmd:
+		if len(args) != 1 {
+			addrCmd.Usage()
+			return
+		}
+		addr, err := nanos.GetAddress(parseIndex(args[0]))
+		if err != nil {
+			log.Fatalln("Couldn't get address:", err)
+		}
+		fmt.Println(addr)
+	case privCmd:
+		if len(args) != 1 {
+			privCmd.Usage()
+			return
+		}
+		priv, err := nanos.GetPrivateKey(parseIndex(args[0]))
+		if err != nil {
+			log.Fatalln("Couldn't get address:", err)
+		}
+		fmt.Println(priv)
+	case getViewKeyCmd:
+		err := nanos.GetViewKey()
+		if err != nil {
+			log.Fatalln(err)
+		}
+	case importPrivateKeyCmd:
+		err := nanos.ImportPrivateKey()
+		if err != nil {
+			log.Fatalln(err)
+		}
+	case genCommitmentCmd:
+		err := nanos.GenCommitment()
+		if err != nil {
+			log.Fatalln(err)
+		}
+	case genOTACmd:
+		err := nanos.GenOTA()
+		if err != nil {
+			log.Fatalln(err)
+		}
+	case genRingSigCmd:
+		err := nanos.GenRingSig()
+		if err != nil {
+			log.Fatalln(err)
+		}
+	case genProofCmd:
+		err := nanos.GenProof()
+		if err != nil {
+			log.Fatalln(err)
+		}
+	case genAssetTagCmd:
+		err := nanos.GenAssetTag()
+		if err != nil {
+			log.Fatalln(err)
+		}
+	case genKeyImageCmd:
+		err := nanos.GenKeyImage()
+		if err != nil {
+			log.Fatalln(err)
+		}
+	case encryptCoinCmd:
+		err := nanos.EncryptCoin()
+		if err != nil {
+			log.Fatalln(err)
+		}
+	case decryptCoinCmd:
+		err := nanos.DecryptCoin()
+		if err != nil {
+			log.Fatalln(err)
+		}
+	case getValidatorCmd:
+		err := nanos.GetValidatorKey()
+		if err != nil {
+			log.Fatalln(err)
+		}
+	case getOTAKeyCmd:
+		err := nanos.GetValidatorKey()
+		if err != nil {
+			log.Fatalln(err)
+		}
+	case trustDeviceCmd:
+		err := nanos.TrustDevice()
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
+}
