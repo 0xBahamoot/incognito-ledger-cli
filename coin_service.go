a4a08bebfdccb5400560f4bd73b5b4bec8d270b7
0xBahamoot <lam@autonomous.nyc>
2021-01-26 15:28:52 +0700

    .

--- coin_service.go
+++ coin_service.go
@@ -1,1 +1,20 @@
 package main
+
+func getAccountList() (map[string]string, error) {
+	result := make(map[string]string)
+	return result, nil
+}
+
+func updateKeyimages(km map[string][]byte) error {
+	return nil
+}
+
+func getEncryptKeyImages(accountName string) (map[string]map[string][]byte, error) {
+	result := make(map[string]map[string][]byte)
+	return result, nil
+}
+
+func getAccountBalance(accountName string) (map[string]uint64, error) {
+	result := make(map[string]uint64)
+	return result, nil
+}
